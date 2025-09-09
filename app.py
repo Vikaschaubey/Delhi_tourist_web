a6e4f7afcedfbd0d136b8159f3b2de95ee14d98c
Vikash <59873405+Vikaschaubey@users.noreply.github.com>
2025-09-09 15:46:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
 db=mysql.connector.connect(
-    host=os.getenv("DB_HOST")
-    user=os.getenv("DB_USERNAME")
-    password=os.getenv("DB_PASSWORD")
-    database=os.getenv("DB_NAME")
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
     port=int(os.getenv("DB_PORT", 3360))
 )
     
